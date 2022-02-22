77faf01dc698677ebcc163cf12e7f04fcd66a291
Markus Gumbel <m.gumbel@hs-mannheim.de>
2022-02-22 14:34:39 +0100

    better parser: recognizes matching labels

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
 // Parser
 
+// Issues: none
+
 use crate::util::Setting;
 use std::collections::HashMap;
 use std::fs::File;
@@ -27,6 +29,12 @@ pub fn parse_write(filepath: &Path, dir_path: &Path, setting: &Setting) -> Resul
 /// as the `Record` value.
 pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, Record>, String> {
     let no_lines = lines.len(); // of the the source file
+    let mut mode = Mode {
+        out: false,
+        exc: false,
+        excsubst: false,
+        var: false,
+    };
 
     // Output collector.
     // Key: a label taken from the annotated source code.
@@ -35,13 +43,14 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
 
     let mut quiet = false; // if true, lines are omitted.
     let mut exercise_quiet = false; // if true, lines are not omitted.
-    // This is the default snippet for extracting the whole source code.
+                                    // This is the default snippet for extracting the whole source code.
     start(DEFAULTLABEL.to_string(), &mut coll);
 
     // Process line by line...
     for (counter, line) in lines.iter().enumerate() {
-        // Show the line and its number.
-        // println!("{}. {}", counter + 1, line);
+        let line_no = counter + 1; // counter starts at 0.
+                                   // Show the line and its number.
+                                   // println!("{}. {}", counter + 1, line);
 
         let _ = match test_token(&line, &setting) {
             // see if this line is a token.
@@ -49,7 +58,7 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
                 println!("  + {}", label); // begin of a code snippet.
                                            // label is moved into hash map:
                 start(label.to_string(), &mut coll);
-                if coll.get(&label).unwrap().counter <= 1 && counter > 1 {
+                if coll.get(&label).unwrap().counter <= 1 && line_no > 1 {
                     // Print ... but not at the beginning of the file
                     // or when ... was printed at the end of a code snippet.
                     coll.get_mut(&label).unwrap().buffer.push_str("...\n");
@@ -62,27 +71,43 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
             }) => {
                 println!("  - {}", label); // end of a code snippet.
                 end(label.to_string(), &mut coll);
-                if counter < no_lines {
+                if line_no < no_lines {
                     // Print ... but not at the end of the file.
                     coll.get_mut(&label).unwrap().buffer.push_str("...\n");
                 }
                 ()
             }
             Some(Token::QuietToken { start: true }) => {
+                if mode.out {
+                    return Err(format!("Line {}: Another +OUT", line_no));
+                }
+                mode.out = true;
                 quiet = true; // start to omit output.
                 ()
             }
             Some(Token::QuietToken { start: false }) => {
+                if !mode.out {
+                    return Err(format!("Line {}: -OUT without preceding +OUT", line_no));
+                }
+                mode.out = false;
                 quiet = false; // end omitting output.
                 ()
             }
             Some(Token::ExerciseToken { start: true }) => {
                 println!("  +EXC");
+                if mode.exc {
+                    return Err(format!("Line {}: Another +EXC", line_no));
+                }
+                mode.exc = true;
                 exercise_quiet = true; // start to omit output in exercise mod.
                 ()
             }
             Some(Token::ExerciseToken { start: false }) => {
                 println!("  -EXC");
+                if !mode.exc {
+                    return Err(format!("Line {}: -EXC without preceding +EXC", line_no));
+                }
+                mode.exc = false;
                 exercise_quiet = false; // end omitting output in exercise mod.
                 ()
             }
@@ -90,6 +115,10 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
                 s: text,
                 start: true,
             }) => {
+                if mode.var {
+                    return Err(format!("Line {}: Another +VAR", line_no));
+                }
+                mode.var = true;
                 for r in coll.values_mut() {
                     // all records
                     if r.active {
@@ -101,6 +130,10 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
                 ()
             }
             Some(Token::ReplaceToken { s: _, start: false }) => {
+                if !mode.var {
+                    return Err(format!("Line {}: -VAR without preceding +VAR", line_no));
+                }
+                mode.var = false;
                 quiet = false; // end marker, output is allowed again.
                 ()
             }
@@ -109,6 +142,10 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
                 start: true,
             }) => {
                 println!("  +EXCSUBST");
+                if mode.excsubst {
+                    return Err(format!("Line {}: Another +EXCSUBST", line_no));
+                }
+                mode.excsubst = true;
                 if !setting.exercise_solution && !quiet {
                     for r in coll.values_mut() {
                         if r.active {
@@ -122,6 +159,13 @@ pub fn parse(lines: &Vec<String>, setting: &Setting) -> Result<HashMap<String, R
             }
             Some(Token::ExerciseReplaceToken { s: _, start: false }) => {
                 println!("  -EXCSUBST");
+                if !mode.excsubst {
+                    return Err(format!(
+                        "Line {}: -EXCSUBST without preceding +EXCSUBST",
+                        line_no
+                    ));
+                }
+                mode.excsubst = false;
                 quiet = false; // end marker, output is allowed again.
                 ()
             }
@@ -157,6 +201,23 @@ fn is_comment_escape(text: &str, setting: &Setting) -> bool {
 fn test_token<'a>(line: &'a str, setting: &'a Setting) -> Option<Token> {
     // let line = line.clone();
     let tokens: Vec<&str> = line.trim().split_whitespace().collect();
+
+    // Rest of line for EXC* tokens:
+    let rest_of_line = || {
+        let indent: i32 = tokens[2].parse().unwrap_or_default();
+        // Truncate first three tokens and fill them with spaces:
+        let mut spaces = if indent == 0 {
+            String::new() // empty string
+        } else {
+            let c: Vec<char> = (1..indent).into_iter().map(|_| ' ').collect();
+            String::from_iter(c)
+        };
+        // Find rest of line:
+        let p = format!("{} {}", tokens[1], tokens[2]);
+        let idx = line.find(&p).unwrap() + p.chars().count() + 1;
+        spaces.push_str(&line[idx..]); // Add rest of line.
+        spaces
+    };
 
     if tokens.len() == 2 {
         if is_comment_escape(tokens[0], setting) && tokens[1] == "+OUT" {
@@ -210,30 +271,14 @@ fn test_token<'a>(line: &'a str, setting: &'a Setting) -> Option<Token> {
             return Some(Token::ReplaceToken { s: s, start: true });
         }
         if is_comment_escape(tokens[0], setting) && tokens[1] == "+VAR" {
-            let indent: i32 = tokens[2].parse().unwrap();
-            // Truncate first three tokens and fill them with spaces:
-            let c: Vec<char> = (1..indent).into_iter().map(|_| ' ').collect();
-            let mut spaces = String::from_iter(c);
-            let idx = tokens[0].len() + tokens[1].len() + tokens[2].len();
-            let t = &line[idx..];
-            spaces.push_str(t);
             return Some(Token::ReplaceToken {
-                s: spaces,
+                s: rest_of_line(),
                 start: true,
             });
         }
-        // TODO almost redundant to previous section.
         if is_comment_escape(tokens[0], setting) && tokens[1] == "+EXCSUBST" {
-            let indent: i32 = tokens[2].parse().unwrap();
-            // Truncate first three tokens and fill them with spaces:
-            let c: Vec<char> = (1..indent).into_iter().map(|_| ' ').collect();
-            let mut spaces = String::from_iter(c);
-            // Find rest of line:
-            let p = format!("{} {}", tokens[1], tokens[2]);
-            let idx = line.find(&p).unwrap() + p.chars().count();
-            spaces.push_str(&line[idx..]); // Add rest of line.
             return Some(Token::ExerciseReplaceToken {
-                s: spaces,
+                s: rest_of_line(),
                 start: true,
             });
         }
@@ -252,14 +297,19 @@ fn start(label: String, coll: &mut HashMap<String, Record>) {
 
 fn end(label: String, coll: &mut HashMap<String, Record>) -> Result<(), String> {
     if !coll.contains_key(&label) {
-        Result::Err("End without start.".to_string())
+        Result::Err(format!("End (-) without start (+) for label: {}", label))
     } else {
         coll.get_mut(&label).unwrap().active = false;
         Result::Ok(())
     }
 }
 
-fn write_files(filepath: &Path, dir_path: &Path, coll: &HashMap<String, Record>, setting: &Setting) {
+fn write_files(
+    filepath: &Path,
+    dir_path: &Path,
+    coll: &HashMap<String, Record>,
+    setting: &Setting,
+) {
     for (label, record) in &*coll {
         // println!("\nFile {}", label);
         // println!("{}", record.buffer);
@@ -268,24 +318,21 @@ fn write_files(filepath: &Path, dir_path: &Path, coll: &HashMap<String, Record>,
         let suffix = filepath.extension().unwrap();
 
         if label == DEFAULTLABEL {
-            let mut file1 = PathBuf::new();
-            file1.push(setting.snippet_dest_dir);
-            file1.push(filepath.file_name().unwrap());
-            write_file(&file1, record);
+            // Write snippet:
+            let snippet_file = setting.snippet_dest_dir.join(filename);
+            write_file(&snippet_file, record);
             // Also write full file to src dest:
-            let file = dir_path.join(filename);
-            write_file(&file, record);
+            let full_file = dir_path.join(filename);
+            write_file(&full_file, record);
         } else {
-            let mut path = PathBuf::new();
-            path.push(setting.snippet_dest_dir);
-            let mut ext_filename = String::new();
-            ext_filename.push_str(&filename.to_str().unwrap());
-            ext_filename.push_str("_");
-            ext_filename.push_str(&label);
-            ext_filename.push_str(".");
-            ext_filename.push_str(suffix.to_str().unwrap());
-            path.push(&ext_filename);
-            write_file(&path, record);
+            let ext_filename = format!(
+                "{}_{}.{}",
+                filename.to_str().unwrap(),
+                label,
+                suffix.to_str().unwrap()
+            );
+            let file = setting.snippet_dest_dir.join(ext_filename);
+            write_file(&file, record);
         }
     }
 }
@@ -297,6 +344,7 @@ fn write_file(filepath: &Path, record: &Record) {
 /// @param active  true if lines are printed.
 /// @param counter number of code snippets (until now)
 /// @param buffer  buffer to collect the output text.
+#[derive(PartialEq, Debug)]
 pub struct Record {
     pub active: bool,
     pub counter: i32,
@@ -313,6 +361,13 @@ impl Record {
     }
 }
 
+struct Mode {
+    out: bool,
+    var: bool,
+    exc: bool,
+    excsubst: bool,
+}
+
 /// `label`: name of the token, `start`: start or end?,
 /// `s`: next line is replaced with this text
 enum Token {
@@ -327,11 +382,11 @@ enum Token {
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
-    use indoc::indoc;
     use crate::parser::parse;
     use crate::parser::DEFAULTLABEL;
     use crate::util::Setting;
+    use indoc::indoc;
+    use std::path::Path;
 
     fn str_to_vec(s: &str) -> Vec<String> {
         let lines = s.split("\n");
@@ -344,9 +399,9 @@ mod tests {
 
     fn str_to_vec2(s: &str) -> Vec<&str> {
         s.split("\n").collect()
-    }    
+    }
 
-    fn config() -> Setting<'static> {
+    fn config_public() -> Setting<'static> {
         // Path is relative to project root.
         Setting {
             src_dir: Path::new("tests/testfiles/src"),
@@ -359,90 +414,163 @@ mod tests {
         }
     }
 
+    fn config_solution() -> Setting<'static> {
+        // Path is relative to project root.
+        Setting {
+            src_dir: Path::new("tests/testfiles/src"),
+            snippet_dest_dir: Path::new("tests/testfiles/snippets"),
+            src_dest_dir: Path::new("tests/testfiles/src_dest"),
+            file_suffix: ".java",
+            comment: "//",
+            comment_alternative: "#",
+            exercise_solution: true,
+        }
+    }
+
     #[test]
     fn test_slide() {
         let s = indoc! {"
-        public class Foo {
-            public static void main(String[] args) {
-                // +IN Slide
-                int a = 1;
-                // -IN Slide
-                System.out.println(\"Value is \" + a);
-            }
-        }
+        line 1
+        // +IN Slide
+          line 3
+        // -IN Slide
+        line 5
         "};
-        let ok = "...\n        int a = 1;\n...\n";
+        let ok = "...\n  line 3\n...\n";
         let lines = str_to_vec(s);
-        let coll = parse(&lines, &config()).unwrap();
+        let coll = parse(&lines, &config_public()).unwrap();
         let test = coll.get("Slide").unwrap().buffer.as_str();
         assert_eq!(coll.len(), 2);
         assert_eq!(test, ok);
     }
 
     #[test]
-    fn test_excsubst() {
+    fn excsubst() {
         let s = indoc! {"
-        public class Foo {
-          public static void main(String[] args) {
-            int a = 1;
-            // +EXCSUBST 4 // Your solution:
-            // This is the solution:
-            System.out.println(\"Value is \" + a);
-            // -EXCSUBST
-          }
-        }
+        line 1
+        // +EXCSUBST 0 // line hint
+        line 3
+        // -EXCSUBST
+        line 5
         "};
         let ok = indoc! {"
-        public class Foo {
-          public static void main(String[] args) {
-            int a = 1;
-            // Your solution:
-          }
-        }
+        line 1
+        // line hint
+        line 5
 
         "};
         // test produces an extra line, therefore this extra line.
         let lines = str_to_vec(s);
-        let coll = parse(&lines, &config()).unwrap();
+        let coll = parse(&lines, &config_public()).unwrap();
         assert_eq!(coll.len(), 1);
         let test = coll.get(DEFAULTLABEL).unwrap().buffer.as_str();
         assert_eq!(test, ok);
     }
 
     #[test]
-    fn test_excsubst_solution() {
-        let mut config = config();
-        config.exercise_solution = true;
+    fn excsubst_solution() {
         let s = indoc! {"
-        public class Foo {
-          public static void main(String[] args) {
-            int a = 1;
-            // +EXCSUBST 4 // Your solution:
-            // This is the solution:
-            System.out.println(\"Value is \" + a);
-            // -EXCSUBST
-          }
-        }
+        line 1
+        // +EXCSUBST 0 // line hint
+        line solution
+        // -EXCSUBST
+        line 5
         "};
         let ok = indoc! {"
-        public class Foo {
-          public static void main(String[] args) {
-            int a = 1;
-            // This is the solution:
-            System.out.println(\"Value is \" + a);
-          }
-        }
+        line 1
+        line solution
+        line 5
 
         "};
         // test produces an extra line, therefore this extra line.
         let lines = str_to_vec(s);
-        let coll = parse(&lines, &config).unwrap();
+        let coll = parse(&lines, &config_solution()).unwrap();
         assert_eq!(coll.len(), 1);
         let test = coll.get(DEFAULTLABEL).unwrap().buffer.as_str();
         assert_eq!(test, ok);
     }
 
     #[test]
-    fn test_parse() {
+    fn unbalanced_out_on() {
+        let s = indoc! {"
+        line 1
+        // +OUT
+        line 3
+        // +OUT
+        line 5
+        "};
+        let lines = str_to_vec(s);
+        let r = parse(&lines, &config_public());
+        assert_eq!(r, Err("Line 4: Another +OUT".to_string()));
+        // assert_eq!(r, Ok(_);
+    }
+
+    #[test]
+    fn unbalanced_out_off() {
+        let s = indoc! {"
+        line 1
+        // -OUT
+        line 3
+        "};
+        let lines = str_to_vec(s);
+        let r = parse(&lines, &config_public());
+        assert_eq!(r, Err("Line 2: -OUT without preceding +OUT".to_string()));
+    }
+
+    #[test]
+    fn unbalanced_exc_on() {
+        let s = indoc! {"
+        line 1
+        // +EXC
+        line 3
+        // +EXC
+        line 5
+        "};
+        let lines = str_to_vec(s);
+        let r = parse(&lines, &config_public());
+        assert_eq!(r, Err("Line 4: Another +EXC".to_string()));
+    }
+
+    #[test]
+    fn unbalanced_exc_off() {
+        let s = indoc! {"
+        line 1
+        // -EXC
+        line 3
+        "};
+        let lines = str_to_vec(s);
+        let r = parse(&lines, &config_public());
+        assert_eq!(r, Err("Line 2: -EXC without preceding +EXC".to_string()));
+        // assert_eq!(r, Ok(_);
+    }
+
+    #[test]
+    fn unbalanced_excsubst_on() {
+        let s = indoc! {"
+        line 1
+        // +EXCSUBST 0 hint
+        line 3
+        // +EXCSUBST 0 hint
+        line 5
+        "};
+        let lines = str_to_vec(s);
+        let r = parse(&lines, &config_public());
+        assert_eq!(r, Err("Line 4: Another +EXCSUBST".to_string()));
+    }
+
+    #[test]
+    fn unbalanced_excsubst_off() {
+        let s = indoc! {"
+        line 1
+        // -EXCSUBST
+        line 3
+        "};
+        let lines = str_to_vec(s);
+        let r = parse(&lines, &config_public());
+        assert_eq!(
+            r,
+            Err("Line 2: -EXCSUBST without preceding +EXCSUBST".to_string())
+        );
+        // assert_eq!(r, Ok(_);
     }
 }
